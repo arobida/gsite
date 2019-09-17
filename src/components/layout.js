4bd8ca45e310d62765a672fce8a7195db225390e
arobida <afrobida@me.com>
2019-09-17 14:34:43 -0700

    put hook right into component

--- src/components/layout.js
+++ src/components/layout.js
@@ -5,7 +5,7 @@
  * See: https://www.gatsbyjs.org/docs/use-static-query/
  */
 
-import React from "react"
+import React,{useState,useEffect} from "react"
 import PropTypes from "prop-types"
 import { useStaticQuery, graphql } from "gatsby"
 // Global Styles & Theme
@@ -13,7 +13,7 @@ import { ThemeProvider } from "styled-components"
 import { GlobalStyles } from "../components/styles/global"
 import { theme } from "../components/styles/theme"
 // Hooks
-import useMedia from "../hooks/useMedia"
+// import useMedia from "../hooks/useMedia"
 // Components
 import Header from "./header"
 import Footer from "./footer"
@@ -29,6 +29,28 @@ const Layout = ({ children }) => {
       }
     }
   `)
+  const useMedia = (query, defaultState = false) => {
+    const [state, setState] = useState(defaultState)
+
+    useEffect(() => {
+      let mounted = true
+      const mql = window.matchMedia(query)
+      const onChange = () => {
+        if (!mounted) return
+        setState(!!mql.matches)
+      }
+
+      mql.addListener(onChange)
+      setState(mql.matches)
+
+      return () => {
+        mounted = false
+        mql.removeListener(onChange)
+      }
+    }, [query])
+
+    return state
+  }
   const isMobile = useMedia("(max-width:500px)")
 
   return (
