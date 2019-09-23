c06eaae50c151021e2f5dde42b977a19e8daf68d
arobida <afrobida@me.com>
2019-09-23 14:01:49 -0700

    changed mobile layout for home page

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,38 +1,47 @@
 import React from "react"
 import { Link } from "gatsby"
-
+import useMedia from "../hooks/useMedia"
 import Layout from "../components/layout"
 import BgImage from "../components/bgImage"
 import SEO from "../components/seo"
 import Button from "../components/button"
 
-const IndexPage = () => (
-  <Layout>
-    <SEO title="Home" />
-    <div style={{ marginTop: "-4em", marginBottom: `1.45rem`, margin: "auto" }}>
-      <BgImage />
-      <h1
-        style={{
-          transform: "translateY(-750px)",
-          fontSize: "40px",
-          WebkitTextStroke: "1px black",
-          color: "white",
-          textShadow:
-            "3px 3px 0 #000,-1px -1px 0 #000,1px -1px 0 #000,-1px 1px 0 #000, 1px 1px 0 #000",
-        }}
+const IndexPage = () => {
+  const mobile = useMedia("(max-width:500px)")
+  return (
+    <Layout>
+      <SEO title="Home" />
+      <div
+        style={{ marginTop: `${mobile?"-3em":"0"}`, marginBottom: `1.45rem`, margin: "auto" }}
       >
-        Hello World
-      </h1>
-      <div style={{ transform: "translateY(-220px)" }}>
-        <Link to="/about">
-          <Button fontSize={1.7}>Learn More</Button>
-        </Link>
+        <BgImage />
+        {
+          mobile ?(
+        <h1
+          style={{
+            transform: "translateY(-750px)",
+            fontSize: "40px",
+            WebkitTextStroke: "1px black",
+            color: "white",
+            textShadow:
+              "3px 3px 0 #000,-1px -1px 0 #000,1px -1px 0 #000,-1px 1px 0 #000, 1px 1px 0 #000",
+          }}
+        >
+          Hello World
+        </h1>
+          ) :null
+        }
+        <div style={{ transform: `${mobile?"translateY(-60px)":"translateY(-220px)"}` }}>
+          <Link to="/about">
+            <Button fontSize={1.7}>Learn More</Button>
+          </Link>
+        </div>
       </div>
-    </div>
 
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-  </Layout>
-)
+      <p>Welcome to your new Gatsby site.</p>
+      <p>Now go build something great.</p>
+    </Layout>
+  )
+}
 
 export default IndexPage
